261b7b0365901796b8c89f85de5cda3eea2886ba
lukaszkalemba <lukasz.kalemba1@gmail.com>
2020-09-30 21:34:26 +0200

    Configure scss modules

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,7 +1,12 @@
 import React, { ReactNode } from 'react';
+import styles from './Button.module.scss';
 
 const Button: React.FC<ButtonProps> = ({ children }) => {
-  return <button type="button">{children}</button>;
+  return (
+    <button type="button" className={styles.button}>
+      {children}
+    </button>
+  );
 };
 
 export interface ButtonProps {
