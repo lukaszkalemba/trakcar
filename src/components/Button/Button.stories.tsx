d7b10d4cf4d49737245b1e5a4e5af968f1e9176b
lukaszkalemba <lukasz.kalemba1@gmail.com>
2020-09-30 21:46:38 +0200

    Configure storybook

---
+++ src/components/Button/Button.stories.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react/types-6-0';
+
+import Button, { ButtonProps } from './Button';
+
+export default {
+  title: 'Atoms/Button',
+  component: Button,
+  argTypes: {},
+} as Meta;
+
+const Template: Story<ButtonProps> = (args) => (
+  <Button {...args}>button</Button>
+);
+
+export const Primary = Template.bind({});
+Primary.args = {};
