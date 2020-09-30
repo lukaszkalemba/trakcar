36ff0a94e2a945a00c1b18024b33172fec236be0
lukaszkalemba <lukasz.kalemba1@gmail.com>
2020-09-30 21:39:21 +0200

    Install types for jest and add test command to precommit hook

---
+++ src/components/Button/Button.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Button from './Button';
+
+describe('<Button />', () => {
+  it('renders correctly', () => {
+    const { debug } = render(<Button>test</Button>);
+
+    debug();
+  });
+});
