44d06d3691746385479328ce185d2cadf777f899
lukaszkalemba <lukasz.kalemba1@gmail.com>
2020-10-11 17:01:53 +0200

    Create and import calendar page file

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
-import App from './App';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Calendar from 'pages/Calendar';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+  return (
+    <React.StrictMode>
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Calendar />
+          </Route>
+        </Switch>
+      </Router>
+    </React.StrictMode>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ src/pages/Calendar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Calendar: React.FC = () => {
+  return (
+    <div>
+      <h1>Calendar.js</h1>
+    </div>
+  );
+};
+
+export default Calendar;
