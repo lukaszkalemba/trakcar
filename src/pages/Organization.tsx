f5d9de8c03f8e450a3ce6725e57dda3afd8ad76a
lukaszkalemba <lukasz.kalemba1@gmail.com>
2020-10-11 18:19:48 +0200

    Create and import positions, organization and profile pages files

---
+++ src/pages/Organization.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Organization: React.FC = () => {
+  return (
+    <div>
+      <h1>Organization.js</h1>
+    </div>
+  );
+};
+
+export default Organization;
---
+++ src/pages/Positions.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Positions: React.FC = () => {
+  return (
+    <div>
+      <h1>Positions.js</h1>
+    </div>
+  );
+};
+
+export default Positions;
