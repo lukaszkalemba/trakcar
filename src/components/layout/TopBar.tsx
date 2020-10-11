b8c71a79b7f1608922bd2e8ca49591a18406b88c
lukaszkalemba <lukasz.kalemba1@gmail.com>
2020-10-11 21:38:06 +0200

    Import logo svg to the top bar component

--- src/components/layout/TopBar.tsx
+++ src/components/layout/TopBar.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import logo from 'assets/svgs/logo_trakcar.svg';
 import styles from './TopBar.module.scss';
 
 const TopBar: React.FC = () => {
-  return <div className={styles.wrapper}>TopBar.js</div>;
+  return (
+    <div className={styles.wrapper}>
+      <img src={logo} alt="" />
+    </div>
+  );
 };
 
 export default TopBar;
