d0a5e582c02e55564c1fa1192789d9667c5924c2
lukaszkalemba <lukasz.kalemba1@gmail.com>
2020-10-12 12:35:08 +0200

    Create user component and styles for it

---
+++ src/components/layout/topBar/User.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Icon from 'components/Icon';
+import userImage from 'assets/images/image_user.png';
+import expandArrow from 'assets/svgs/icon_expand-arrow.svg';
+import styles from './User.module.scss';
+
+const User: React.FC = () => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.avatarWrapper}>
+        <img src={userImage} className={styles.avatar} alt="user avatar" />
+      </div>
+      <div className={styles.user}>
+        <div className={styles.username}>Julia Smith</div>
+        <Icon className={styles.expandArrow} src={expandArrow} />
+      </div>
+    </div>
+  );
+};
+
+export default User;
