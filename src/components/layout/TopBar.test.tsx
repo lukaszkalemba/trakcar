acd8ea4f7961148070f5fa97985575dd447a805f
lukaszkalemba <lukasz.kalemba1@gmail.com>
2020-10-12 14:52:35 +0200

    Import style files to the storybook preview file

--- src/components/layout/TopBar.test.tsx
+++ src/components/layout/TopBar.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import TopBar from '../TopBar';
+import TopBar from './TopBar';
 
 describe('<TopBar />', () => {
   it('renders correctly', () => {
