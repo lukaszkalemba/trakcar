8f39f458e1678a7f6a545f5f7531fbadc861cd08
lukaszkalemba <lukasz.kalemba1@gmail.com>
2020-10-12 12:35:08 +0200

    Create user component and styles for it

--- src/components/layout/TopBar.tsx
+++ src/components/layout/TopBar.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Icon from 'components/Icon';
 import logo from 'assets/svgs/logo_trakcar.svg';
+import User from './topBar/User';
 import styles from './TopBar.module.scss';
 
 const TopBar: React.FC = () => {
   return (
-    <div className={styles.topBar}>
+    <div className={styles.wrapper}>
       <Icon className={styles.logo} src={logo} />
+      <User />
     </div>
   );
 };
