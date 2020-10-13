a443401efe45a9a2f8f1be4a2cdb43da486c9f93
lukaszkalemba <lukasz.kalemba1@gmail.com>
2020-10-13 22:06:05 +0200

    Rename arrow down svg

--- src/components/layout/top-bar/user/User.tsx
+++ src/components/layout/top-bar/user/User.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Icon from 'components/icon/Icon';
 import userImage from 'assets/images/image_user.png';
-import expandArrow from 'assets/svgs/icon_expand-arrow.svg';
+import arrowDown from 'assets/svgs/icon_arrow-down.svg';
 import styles from './User.module.scss';
 
 const User: React.FC = () => {
@@ -12,7 +12,7 @@ const User: React.FC = () => {
       </div>
       <div className={styles.user}>
         <div className={styles.username}>Julia Smith</div>
-        <Icon className={styles.expandArrow} src={expandArrow} />
+        <Icon className={styles.expandArrow} src={arrowDown} />
       </div>
     </div>
   );
