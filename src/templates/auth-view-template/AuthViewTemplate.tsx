716f72058fc2989fde543a922f8498b96a6debf9
lukaszkalemba <lukasz.kalemba1@gmail.com>
2020-10-13 21:33:42 +0200

    Create styles module for auth view template

--- src/templates/auth-view-template/AuthViewTemplate.tsx
+++ src/templates/auth-view-template/AuthViewTemplate.tsx
@@ -1,7 +1,19 @@
 import React, { ReactNode } from 'react';
+import Icon from 'components/icon/Icon';
+import logo from 'assets/svgs/logo_trakcar.svg';
+import styles from './AuthViewTemplate.module.scss';
 
 const AuthViewTemplate: React.FC<AuthViewTemplateProps> = ({ children }) => {
-  return <div>{children}</div>;
+  return (
+    <div className={styles.wrapper}>
+      <main>
+        <div className={styles.board}>
+          <Icon src={logo} />
+          {children}
+        </div>
+      </main>
+    </div>
+  );
 };
 
 interface AuthViewTemplateProps {
