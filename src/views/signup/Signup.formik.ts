7ab5ab01cf53c32480016956aba4588e8e8a48a6
lukaszkalemba <lukasz.kalemba1@gmail.com>
2020-10-14 18:43:41 +0200

    Update signup validation schema messages

--- src/views/signup/Signup.formik.ts
+++ src/views/signup/Signup.formik.ts
@@ -13,9 +13,18 @@ export const initialValues: SignupValues = {
 };
 
 export const validationSchema = yup.object({
-  name: yup.string().min(5).required(),
-  email: yup.string().email().required(),
-  password: yup.string().min(6).required(),
+  name: yup
+    .string()
+    .min(5, 'Name must be at least 5 characters')
+    .required('Name is required'),
+  email: yup
+    .string()
+    .email('Email must be valid')
+    .required('Email is required'),
+  password: yup
+    .string()
+    .min(6, 'Password must be at least 6 characters')
+    .required('Password is required'),
 });
 
 export const onSubmit = (values: SignupValues) => {
