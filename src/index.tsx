70e95b776891b97e3b27a3498209ee9ece211876
lukaszkalemba <lukasz.kalemba1@gmail.com>
2020-10-17 11:38:51 +0200

    Configure redux store

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { configureStore, Action } from '@reduxjs/toolkit';
+import { ThunkAction } from 'redux-thunk';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Calendar from 'views/calendar/Calendar';
 import Positions from 'views/positions/Positions';
@@ -11,20 +14,29 @@ import NotFound from 'views/not-found/NotFound';
 import 'normalize.css';
 import 'styles/global.scss';
 
+export type AppThunk = ThunkAction<void, null, unknown, Action<string>>;
+
+const store = configureStore({
+  reducer: {},
+  devTools: process.env.NODE_ENV === 'development',
+});
+
 const App = () => {
   return (
     <React.StrictMode>
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Calendar} />
-          <Route exact path="/positions" component={Positions} />
-          <Route exact path="/organization" component={Organization} />
-          <Route exact path="/profile" component={Profile} />
-          <Route exact path="/sign-up" component={SignUp} />
-          <Route exact path="/sign-in" component={SignIn} />
-          <Route component={NotFound} />
-        </Switch>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Calendar} />
+            <Route exact path="/positions" component={Positions} />
+            <Route exact path="/organization" component={Organization} />
+            <Route exact path="/profile" component={Profile} />
+            <Route exact path="/sign-up" component={SignUp} />
+            <Route exact path="/sign-in" component={SignIn} />
+            <Route component={NotFound} />
+          </Switch>
+        </Router>
+      </Provider>
     </React.StrictMode>
   );
 };
