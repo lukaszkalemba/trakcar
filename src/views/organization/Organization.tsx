cd9873f078910750cd8d0005d11eb6da31c38a62
lukaszkalemba <lukasz.kalemba1@gmail.com>
2020-10-26 11:26:35 +0100

    Create no organization info component

--- src/views/organization/Organization.tsx
+++ src/views/organization/Organization.tsx
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux';
 import { usersSelector } from 'modules/users';
 import Layout from 'components/layout/Layout';
 import User from './user/User';
+import NoOrganizationInfo from './no-organization-info/NoOrganizationInfo';
 import styles from './Organization.module.scss';
 
 const Organization: React.FC = () => {
@@ -14,6 +15,7 @@ const Organization: React.FC = () => {
     <Layout>
       <div className={styles.wrapper}>
         <User />
+        <NoOrganizationInfo />
       </div>
     </Layout>
   );
---
+++ src/views/organization/no-organization-info/NoOrganizationInfo.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import join_icon from 'assets/svgs/icon_join.svg';
+import create_icon from 'assets/svgs/icon_create.svg';
+import Button from 'components/button/Button';
+import styles from './NoOrganizationInfo.module.scss';
+
+const NoOrganizationInfo: React.FC = () => {
+  return (
+    <section className={styles.wrapper}>
+      <h1 className={styles.info}>
+        You are not a member of any organization yet.
+      </h1>
+      <div className={styles.actions}>
+        <Button iconSrc={create_icon}>create new</Button>
+        <Button iconSrc={join_icon}>join existing</Button>
+      </div>
+    </section>
+  );
+};
+
+export default NoOrganizationInfo;
