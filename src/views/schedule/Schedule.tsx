dec16db2a1e4530c656605ab91b5be31b03307ec
lukaszkalemba <lukasz.kalemba1@gmail.com>
2020-11-09 19:28:25 +0100

    Update schedule styles

--- src/views/schedule/Schedule.tsx
+++ src/views/schedule/Schedule.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Layout from 'components/layout/Layout';
-import ButtonsActions from './buttons-actions/ButtonsActions';
+import Actions from './actions/Actions';
 import styles from './Schedule.module.scss';
 
 const Schedule: React.FC = () => {
   return (
     <Layout>
       <div className={styles.wrapper}>
-        <ButtonsActions />
+        <Actions />
       </div>
     </Layout>
   );
--- src/views/schedule/actions/Actions.tsx
+++ src/views/schedule/actions/Actions.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import Icon from 'components/icon/Icon';
 import icon_plus from 'assets/svgs/icon_circle-plus.svg';
 import icon_calendar from 'assets/svgs/icon_calendar-black.svg';
-import styles from './ButtonsActions.module.scss';
+import styles from './Actions.module.scss';
 
-const ButtonsActions: React.FC = () => {
+const Actions: React.FC = () => {
   return (
     <div className={styles.wrapper}>
       <button className={styles.orderButton}>
-        <span>Book order</span>
+        <span>Book an order</span>
         <Icon className={styles.orderIcon} src={icon_plus} />
       </button>
       <button className={styles.calendarButton}>
@@ -18,4 +18,4 @@ const ButtonsActions: React.FC = () => {
   );
 };
 
-export default ButtonsActions;
+export default Actions;
