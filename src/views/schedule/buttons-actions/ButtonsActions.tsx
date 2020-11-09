18ef9b6d2ebde84304f8a3ce929343f47d12ac14
lukaszkalemba <lukasz.kalemba1@gmail.com>
2020-11-09 18:51:30 +0100

    Create and use buttons actions schedule component

--- src/views/schedule/buttons-actions/ButtonsActions.tsx
+++ src/views/schedule/buttons-actions/ButtonsActions.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import Icon from 'components/icon/Icon';
+import icon_plus from 'assets/svgs/icon_circle-plus.svg';
+import icon_calendar from 'assets/svgs/icon_calendar-black.svg';
 import styles from './ButtonsActions.module.scss';
 
 const ButtonsActions: React.FC = () => {
   return (
     <div className={styles.wrapper}>
-      <button className={styles.button}>Book order</button>
-      <button className={styles.button}>Pick date</button>
+      <button className={styles.orderButton}>
+        <span>Book order</span>
+        <Icon className={styles.orderIcon} src={icon_plus} />
+      </button>
+      <button className={styles.calendarButton}>
+        <Icon src={icon_calendar} />
+      </button>
     </div>
   );
 };
