7c5c25d7f5de4c5c95ac730767a39b05b3a694db
lukaszkalemba <lukasz.kalemba1@gmail.com>
2020-11-09 17:22:19 +0100

    Create styles for schedule view with wrapper class inside

--- src/views/schedule/Schedule.tsx
+++ src/views/schedule/Schedule.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Layout from 'components/layout/Layout';
+import styles from './Schedule.module.scss';
 
 const Schedule: React.FC = () => {
   return (
     <Layout>
-      <div>
+      <div className={styles.wrapper}>
         <h1>Schedule.js</h1>
       </div>
     </Layout>
