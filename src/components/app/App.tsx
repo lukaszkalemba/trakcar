4c5767857332a65cf637237f73eefb222e8b81e9
lukaszkalemba <lukasz.kalemba1@gmail.com>
2020-11-16 22:50:47 +0100

    Create routes for every order

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Schedule from 'views/schedule/Schedule';
 import Positions from 'views/positions/Positions';
@@ -7,11 +7,8 @@ import Organization from 'views/organization/Organization';
 import SignUp from 'views/sign-up/SignUp';
 import SignIn from 'views/sign-in/SignIn';
 import NotFound from 'views/not-found/NotFound';
-// import Order from 'views/order/Order';
-import {
-  ordersSelector,
-  // getAllOrders
-} from 'modules/orders';
+import Order from 'views/order/Order';
+import { ordersSelector, Order as OrderSchema } from 'modules/orders';
 import { setFullHeight } from 'helpers/setFullHeight';
 import { setAuthToken } from 'helpers/setAuthToken';
 import Alerts from './alerts/Alerts';
@@ -24,16 +21,26 @@ if (localStorage.token) {
 }
 
 const App = () => {
-  const dispatch = useDispatch();
-  const {
-    // orders,
-    loading,
-  } = useSelector(ordersSelector);
+  const { orders } = useSelector(ordersSelector);
 
   useEffect(() => {
     setFullHeight();
-    // dispatch(getAllOrders());
-  }, [dispatch]);
+  }, []);
+
+  let ordersRoutes = null;
+
+  if (orders?.length) {
+    ordersRoutes = (orders as OrderSchema[]).map((order) => {
+      return (
+        <PrivateRoute
+          key={order._id}
+          exact
+          path={`/orders/${order._id}`}
+          Component={() => <Order {...order} />}
+        />
+      );
+    });
+  }
 
   return (
     <Router>
@@ -44,17 +51,8 @@ const App = () => {
         <PrivateRoute exact path="/organization" Component={Organization} />
         <Route exact path="/sign-up" component={SignUp} />
         <Route exact path="/sign-in" component={SignIn} />
-        {/* {orders.map((order) => {
-          return (
-            <PrivateRoute
-              key={order._id}
-              exact
-              path={`/orders/${order._id}`}
-              Component={() => <Order {...order} />}
-            />
-          );
-        })} */}
-        {!loading && <Route component={NotFound} />}
+        {ordersRoutes}
+        <Route component={NotFound} />
       </Switch>
     </Router>
   );
